cc99863151e9d9a9b9991d9369ef779072f83dfc
Lucas Sousa <frempyrean@gmail.com>
2021-03-30 09:11:17 -0300

    chore: add tsconfig-paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,14 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils'
+import { compilerOptions } from './tsconfig.json'
+
 export default {
   bail: true,
   clearMocks: true,
   coverageProvider: 'v8',
   testEnvironment: 'node',
   preset: 'ts-jest',
-  testMatch: ['**/*.spec.ts']
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src'
+  })
 }
