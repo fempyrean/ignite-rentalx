013ef4c6acc19b4218502b8e07881c9b40501c90
Lucas Sousa <frempyrean@gmail.com>
2021-04-30 09:00:40 -0300

    feat: update db config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -18,13 +18,10 @@ export const createConnectionSQLite = async (): Promise<Connection> => {
   )
 }
 
-export const createConnectionPostgres = async (
-  host = 'localhost'
-): Promise<Connection> => {
+export const createConnectionPostgres = async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions()
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentalx_test'
